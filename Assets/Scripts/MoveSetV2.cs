8c97d93895b284d5055fef3c67348e8aeacfd3e2
AlonsoRial <alonsorivasalcobendas@gmail.com>
2023-07-31 18:14:24 +0200

    controles de switch como ps4 y boton de retroceso

--- Assets/Scripts/MoveSetV2.cs
+++ Assets/Scripts/MoveSetV2.cs
@@ -8,8 +8,12 @@ using UnityEngine.Windows;
 public class MoveSetV2 : MonoBehaviour
 {
     public PlayerInput _playerInput;
-    public float velocidadRotacion;
+    public float speedRotation = 30;
+    public float speedRun=30;
+
     Vector2 movementInput;
+
+    public float rotarSwitch;
 
     private void Start()
     {
@@ -19,12 +23,21 @@ public class MoveSetV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementInput = _playerInput.actions["Move"].ReadValue<Vector2>();
+        //movementInput = _playerInput.actions["Move"].ReadValue<Vector2>();
+        //this.transform.Rotate(Vector3.up * movementInput * Time.deltaTime * velocidadRotacion);
 
-        this.transform.Rotate(Vector3.up * movementInput * Time.deltaTime * velocidadRotacion);
+
+        rotarSwitch = _playerInput.actions["Move"].ReadValue<float>();
+
+        this.transform.Rotate(Vector3.up * rotarSwitch * Time.deltaTime * speedRotation);
 
         if (_playerInput.actions["Run"].IsPressed()) {
-            this.transform.Translate(Vector3.forward * velocidadRotacion * Time.deltaTime);
+            this.transform.Translate(Vector3.forward * speedRun * Time.deltaTime);
+        }
+        else if (_playerInput.actions["NoRun"].IsPressed())
+        {
+            Vector3 auxi = Vector3.forward * speedRun * Time.deltaTime;
+            this.transform.Translate(-auxi);
         }
 
     }
