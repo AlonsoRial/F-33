fab362637089a599f5bcb6492bb1a15a177b9cfc
AlonsoRial <alonsorivasalcobendas@gmail.com>
2023-10-27 23:46:19 +0200

    cronometro e boton faltante implementado del pausa

---
+++ Assets/Scripts/Cronometro.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cronometro : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI textoCrono;
+    [SerializeField] private float tiempo;
+
+    private int minutos, segundos, decimas;
+
+    void Crono() {
+        tiempo += Time.deltaTime;
+
+        minutos = Mathf.FloorToInt(tiempo / 60);
+        segundos = Mathf.FloorToInt(tiempo % 60);
+        decimas = Mathf.FloorToInt((tiempo%1)*100);
+
+        textoCrono.text = string.Format("{0:00}:{1:00}:{2:00}", minutos,segundos,decimas);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Crono();
+    }
+}
